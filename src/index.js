6093ecfa8388c881389b69ae41a589b53fdfa520
Felipe Queiroz <felipe.de.a.queiroz@outlook.com>
2025-07-07 15:12:58 -0300

    Add CORS support to the application

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 // Ponto de entrada principal para Express e Lambda
 const express = require('express');
+const cors = require('cors');
 const logger = require('./middlewares/logger');
 const routes = require('./routes');
 
 const app = express();
 
+// Habilita o CORS para todas as origens. Para produção, é recomendado restringir para a origem do seu frontend.
+app.use(cors());
 app.use(express.json());
 app.use(logger);
 
